9ad9abdf7050b1c571247079a58cb8fe6fa53689
RonyMiah <ronymiah2121@gmail.com>
2021-11-13 17:10:14 +0600

    done

--- src/Components/AboutUs/AboutUs.js
+++ src/Components/AboutUs/AboutUs.js
@@ -3,7 +3,7 @@ import React from 'react';
 const AboutUs = () => {
     return (
         <div className="py-5 my-5 text-center">
-            <h1 >About Us Coming Soon</h1>
+            <h1 > Working On this Page Back to  Coming Very Soon</h1>
         </div>
     );
 };
